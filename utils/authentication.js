b121ef3d6cf109c748f6710d73b95e954e5110ec
Landver1 <stanislav.teitel@crowdbotics.com>
2021-12-15 21:42:12 +0300

    removed commented code

--- utils/authentication.js
+++ utils/authentication.js
@@ -8,21 +8,15 @@ const login = async (page, url, login, password) => {
     await page.click('button.ui-btn', { delay: 300 })
   }
   await page.waitForSelector('#login')
-  // await page.screenshot({ path: './screenshots/1.init.png' })
 
   await page.type('#login', login)
-  // // await page.screenshot({ path: './screenshots/2.after_filling_login.png' })
   await page.click('button.ui-btn', { delay: 300 })
-  // // await page.screenshot({ path: './screenshots/3.after_login_button_click.png' })
 
   await page.waitForSelector('#password')
-  // // await page.screenshot({ path: './screenshots/4.after_navigate_to_pass.png' })
   await page.type('#password', password)
-  // // await page.screenshot({ path: './screenshots/5.after_filling_pass.png' })
   await page.click('button.ui-btn', { delay: 300 })
 
   await page.waitForNavigation()
-  // // await page.screenshot({ path: './screenshots/6.after_login.png' })
 
   console.log('\x1b[32m%s\x1b[0m', 'Login successful!')
 }
